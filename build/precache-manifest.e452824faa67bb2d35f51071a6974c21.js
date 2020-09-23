073b364b954d265562c853e560d85d7acf75e527
pierre <penel.pierre@gmail.com>
2020-09-22 23:10:53 -0400

    .gitignore: allow build

---
+++ build/precache-manifest.e452824faa67bb2d35f51071a6974c21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4aa6e0df1ae9a9ca12058bff69851fb8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b648612a1e896f7644d5",
+    "url": "/static/css/main.1a4f05dc.chunk.css"
+  },
+  {
+    "revision": "3364dd97c29519d36a37",
+    "url": "/static/js/2.d3979ab1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d3979ab1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b648612a1e896f7644d5",
+    "url": "/static/js/main.94dcbc3c.chunk.js"
+  },
+  {
+    "revision": "3c3af423e1406e4f5b2c",
+    "url": "/static/js/runtime-main.c17cd845.js"
+  }
+]);
